f3c83d92147e4847f727218ae4e5eac63282fee9
a7mddarwish <a7mddarwish7Dev@gmail.com>
2025-07-05 15:06:07 +0300

    modify model

--- TaskMS.AccessData/obj/Debug/net8.0/TaskMS.AccessData.AssemblyInfo.cs
+++ TaskMS.AccessData/obj/Debug/net8.0/TaskMS.AccessData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskMS.AccessData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e38788dcc3f6eb078c637aa1c73282008c588e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dcec0f2ed688260d7be0c835ff40a940052771e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskMS.AccessData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskMS.AccessData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
